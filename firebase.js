c873f8efca62e334fb96e21f68c40d7f912880a9
mddaaoo9-ux <mddaaoo9@gmail.com>
2025-12-25 23:58:16 +0200

    Update and rename app.js to firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKck8hyGJqhWnZGF4y6qR1zDWEw7QhxZI",
+  authDomain: "top-stor.firebaseapp.com",
+  projectId: "top-stor",
+  storageBucket: "top-stor.firebasestorage.app",
+  messagingSenderId: "987517761472",
+  appId: "1:987517761472:web:dbbcba53265a02a6ee7a25",
+  measurementId: "G-LF229BBX87"
+};
