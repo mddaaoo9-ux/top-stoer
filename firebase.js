a3424e3999693477b6a91ca671d6330fadf4b383
mddaaoo9-ux <mddaaoo9@gmail.com>
2025-12-26 00:25:14 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDKck8hyGJqhWnZGF4y6qR1zDWEw7QhxZI",
   authDomain: "top-stor.firebaseapp.com",
@@ -8,3 +11,8 @@ const firebaseConfig = {
   appId: "1:987517761472:web:dbbcba53265a02a6ee7a25",
   measurementId: "G-LF229BBX87"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
